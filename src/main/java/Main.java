913b928ea41270bc51b9e0cbd8de52826787b879
Negrustin <negrustin87@gmail.com>
2022-05-30 18:15:30 +0200

    reformat code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,18 @@
 public class Main {
-  public static void main(String[] args) {
-    BonusService service = new BonusService();
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
 
-    long amount = 1000_60;
-    boolean registered = true;
-    
-    long expected = 30;
-    long actual = service.calculate(amount,registered);
+        long amount = 1000_60;
+        boolean registered = true;
 
-    if (actual == expected){
-      System.out.println(actual + " == " + expected + " PASED");
-    }else{
-      System.out.println(actual + " != " + expected + " FAILED");
+        long expected = 30;
+        long actual = service.calculate(amount, registered);
+
+        if (actual == expected) {
+            System.out.println(actual + " == " + expected + " PASED");
+        } else {
+            System.out.println(actual + " != " + expected + " FAILED");
+        }
     }
-  }
 }
 
